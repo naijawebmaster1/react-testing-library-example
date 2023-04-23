cd533cc56af4551366636ea595f3e92a21927857
Clement Omolayo <naijawebmaster1@gmail.com>
2023-04-23 01:54:31 +0100

    ...

--- src/Counter.js
+++ src/Counter.js
@@ -22,8 +22,8 @@ function Counter({ initialCount }) {
         Count: <h3 data-testid="count">{count}</h3>
       </h1>
       <div>
-        <button onClick={increment}> Increment</button>
-        <button onClick={decrement}> Decrement</button>
+        <button onClick={increment}> Increment +</button>
+        <button onClick={decrement}> Decrement -</button>
         <button onClick={restart}> Restart</button>
         <button onClick={switchSigns}> Switch Signs</button>
       </div>
